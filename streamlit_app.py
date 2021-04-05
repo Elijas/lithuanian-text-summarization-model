3324303b432d099c0064627a976929e709714532
tarasevic-r <robert.tarasevic@gmail.com>
2021-04-05 22:25:49 +0300

    load summ model

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,24 @@
 import streamlit as st
+import sentencepiece as spm
+from fairseq.models.bart import BARTModel
 
 #===========================================#
 #                Load Model                 #
 #===========================================#
 
-model = ...
-
+BASEDIR = './model'
+bart = BARTModel.from_pretrained(
+        BASEDIR,
+        checkpoint_file='checkpoint_best.pt',
+        bpe='sentencepiece',
+        sentencepiece_model=BASEDIR + '/sentence.bpe.model')
+bart.eval()
 #===========================================#
 #                 Run Inference             #
 #===========================================#
 
 def summarize(full_text):
-    # summarized_text = model.run(full_text)
-    summarized_text = "Apibendrintas tekstas"
+    summarized_text = bart.sample(full_text, beam=4, lenpen=2.0, max_len_b=140, min_len=55, no_repeat_ngram_size=3)
     return summarized_text
 
 #===========================================#
