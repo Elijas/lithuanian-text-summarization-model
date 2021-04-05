2194ee626ec6009e75d328a47dce0cdc09a74b96
Elijas Dapšauskas <master.elijas@gmail.com>
2021-04-06 00:04:26 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,19 +6,23 @@ from fairseq.models.bart import BARTModel
 #                Load Model                 #
 #===========================================#
 
-BASEDIR = './model'
-bart = BARTModel.from_pretrained(
-        BASEDIR,
-        checkpoint_file='checkpoint_best.pt',
-        bpe='sentencepiece',
-        sentencepiece_model=BASEDIR + '/sentence.bpe.model')
-bart.eval()
+@st.cache
+def get_model():
+    BASEDIR = './model'
+    bart = BARTModel.from_pretrained(
+            BASEDIR,
+            checkpoint_file='checkpoint_best.pt',
+            bpe='sentencepiece',
+            sentencepiece_model=BASEDIR + '/sentence.bpe.model')
+    bart.eval()
+    return bart
+
 #===========================================#
 #                 Run Inference             #
 #===========================================#
 
 def summarize(full_text):
-    summarized_text = bart.sample([full_text], beam=4, lenpen=2.0, max_len_b=140, min_len=55, no_repeat_ngram_size=3)
+    summarized_text = get_model().sample([full_text], beam=4, lenpen=2.0, max_len_b=140, min_len=55, no_repeat_ngram_size=3)
     return summarized_text
 
 #===========================================#
