1e2e99ad849bf1e3e6eba52ab08816356deaddaf
tarasevic-r <robert.tarasevic@gmail.com>
2021-04-06 10:30:20 +0300

    text box changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ def get_model():
 #===========================================#
 
 def summarize(full_text):
-    summarized_text = get_model().sample([full_text], beam=4, lenpen=2.0, max_len_b=140, min_len=55, no_repeat_ngram_size=3)
+    summarized_text = get_model().sample([full_text], beam=5, lenpen=2.0, max_len_b=140, min_len=30, no_repeat_ngram_size=3)
     return summarized_text
 
 #===========================================#
@@ -33,8 +33,10 @@ desc = "Ši programa gali apibendrinti jūsų pateiktą lietuvišką tekstą."
 st.title('Lietuviško teksto automatinis apibendrinimas')
 st.write(desc)
 
-user_input = st.text_input('Jūsų tekstas:')
+#user_input = st.text_input('Jūsų tekstas:')
+user_input = st.text_area(label='Jūsų tekstas:', height=200)
 
 if st.button('Apibendrinti'):
-    summarized_text = summarize(user_input)
-    st.write(summarized_text)
+    summarized_text = summarize(user_input)[0][:-7]
+#    st.write(summarized_text)
+    st.text_area(label='Įvesto teksto santrauka:', value=summarized_text, height=100)
