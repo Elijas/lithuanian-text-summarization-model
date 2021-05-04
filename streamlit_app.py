3e944ad56ec6547149ff6916b531ccd766f6eb47
Robert Tarasevic <robert@neurotech.lt>
2021-05-04 10:12:49 +0300

    c

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,21 +22,22 @@ def get_model():
 #===========================================#
 
 def summarize(full_text):
-    summarized_text = get_model().sample([full_text][:600], beam=7, lenpen=2.0, max_len_b=140, min_len=30, no_repeat_ngram_size=3)
+    summarized_text = get_model().sample([full_text], beam=7, lenpen=2.0, max_len_b=140, min_len=30, no_repeat_ngram_size=3)
     return summarized_text
 
 #===========================================#
 #              Streamlit Code               #
 #===========================================#
-desc = "Ši programa gali apibendrinti jūsų pateiktą lietuvišką tekstą.\n Autorius: Robert Tarasevič, 2021"
-
+desc = "Ši programa gali apibendrinti jūsų pateiktą lietuvišką tekstą."
+authr = "Autorius: Robert Tarasevič, 2021"
 st.title('Lietuviško teksto automatinis apibendrinimas')
 st.write(desc)
+st.write(authr)
 
 #user_input = st.text_input('Jūsų tekstas:')
 user_input = st.text_area(label='Jūsų tekstas:', height=200)
 
 if st.button('Apibendrinti'):
-    summarized_text = summarize(user_input)[0][:-7]
+    summarized_text = summarize(user_input[:600])[0][:-7]
 #    st.write(summarized_text)
     st.text_area(label='Įvesto teksto santrauka:', value=summarized_text, height=100)
